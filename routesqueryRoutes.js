bc91db7005c938d7b051db1a671eaf7c8be5eaa3
Saidahmad <sidahmadsanjarugli@gmail.com>
2026-06-03 16:04:20 +0500

    cleaned

--- routesqueryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const queryController = require('../controllers/queryController');
-
-router.get('/', queryController.getSavedQueries);
-router.get('/stats', queryController.getStats);
-router.delete('/:id', queryController.deleteSavedQuery);
-
-module.exports = router;
